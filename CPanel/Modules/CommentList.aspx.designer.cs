2ad998b4031c46838df402e552ee6083fde85436
Nguyen Cao Hung <bbconcepteur@gmail.com>
2018-12-06 02:33:30 +0700

    uupdate h

--- CPanel/Modules/CommentList.aspx.designer.cs
+++ CPanel/Modules/CommentList.aspx.designer.cs
@@ -58,15 +58,6 @@ namespace CPanel.Modules {
         protected global::System.Web.UI.WebControls.Button btnViewComment;
         
         /// <summary>
-        /// btnViewTranslation control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnViewTranslation;
-        
-        /// <summary>
         /// txtCommentID control.
         /// </summary>
         /// <remarks>
